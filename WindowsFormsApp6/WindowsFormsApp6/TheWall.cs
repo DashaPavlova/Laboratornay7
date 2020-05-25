9d9cde0c490d3ec47f7c169db12f93e8fd653f80
DashaPavlova <60984791+DashaPavlova@users.noreply.github.com>
2020-05-26 00:32:01 +0300

    Create TheWall.cs

---
+++ WindowsFormsApp6/WindowsFormsApp6/TheWall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp6
+{
+    class TheWall
+    {
+
+        public int x;
+        public int y;
+
+        public int sizeX;
+        public int sizeY;
+
+        public Image wallImg;
+
+        public TheWall(int x, int y, bool isRotatedImage = false)
+        {
+            wallImg = new Bitmap(@"Труба.png");
+            this.x = x;
+            this.y = y;
+            sizeX = 150;
+            sizeY = 250;
+            if (isRotatedImage)
+                wallImg.RotateFlip(RotateFlipType.Rotate180FlipX);
+        }
+    }
+}
